0650da584f40402318852e0fcf9221e51c699468
Mariovalcen <mariovalcen@gmail.com>
2023-02-11 20:24:05 +0100

    Show contact list

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -15,7 +15,44 @@
         </div>
 
         @if ($contacts->count())
-        <p>Aquí se mostrará tu lista de contactos</p>
+        
+<div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    Nombre
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Correo electrónico
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Editar
+                </th>
+            
+            </tr>
+        </thead>
+        <tbody>
+
+            @foreach ($contacts as $contact)
+            <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    {{$contact->user->name}}
+                </th>
+                <td class="px-6 py-4">
+                    {{$contact->user->email}}
+                </td>
+                
+                <td class="px-6 py-4">
+                    <a href="{{ route ('contacts.edit', $contact)}}" class="btn btn-blue">Editar</a>
+                </td>
+            </tr>
+
+            @endforeach
+        </tbody>
+    </table>
+</div>
+
         @else
 
         
