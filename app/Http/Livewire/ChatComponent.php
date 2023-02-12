fe28ea6b805adeecd01ba7f3be404b40ef05c172
Mariovalcen <mariovalcen@gmail.com>
2023-02-12 15:52:10 +0100

    Filter contacts

--- app/Http/Livewire/ChatComponent.php
+++ app/Http/Livewire/ChatComponent.php
@@ -3,9 +3,33 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Contact;
 
 class ChatComponent extends Component
+
 {
+
+    public $search;
+
+    // Propiedad computada
+    public function getContactsProperty(){
+        return Contact::where('user_id', auth()->id())
+        ->when($this->search, function($query){
+
+            $query->where(function($query){
+               $query->where('name','like', '&'.$this->search.'&')
+                ->orWhereHas('user', function($query){
+                    $query->where('email','like','%'.$this->search.'%');
+
+                });  
+            });
+
+           
+        })
+        ->get() ?? [];
+
+    }
+
     public function render()
     {
         return view('livewire.chat-component')->layout('layouts.chat');
--- resources/views/livewire/chat-component.blade.php
+++ resources/views/livewire/chat-component.blade.php
@@ -14,12 +14,40 @@
 
             <div class="bg-white h-14 flex items-center px-4">
                 <x-jet-input type="text"
+                    wire:model="search"
                     class="w-full"
                     placeholder="Busque un chat o inicie uno nuevo"/>
             </div>
 
             <div class="h-[calc(100vh-10.5rem)] overflow-auto border-t border-gray-200">
-                
+                <div class="px-4 py-3">
+                    <h2 class="text-teal-600 text-lg mb-4">Contactos</h2>
+
+                <ul class="space-y4">
+                    @forelse ($this->contacts as $contact)
+                    <li class="cursor-pointer p-2">
+                        <div class="flex">
+                            <figure class="flex-shrink-0">
+                                <img class="h-12 w-12 object-cover object-center rounded-full " src="{{ $contact->user->profile_photo_url}}" alt="{{$contact->name}}">
+                            </figure>
+                            <div class="flex-1 ml-5 border-b gray-200">
+                                <p class="text-gray-800">
+                                    {{ $contact->name}}
+                                </p>
+                                <p class="text-gray-400">
+                                    {{ $contact->user->email}}
+                                </p>
+
+                            </div>
+                        </div>
+
+                    </li>
+                    @empty
+
+                    @endforelse
+
+                </ul>
+              </div>  
             </div>
 
         </div>
